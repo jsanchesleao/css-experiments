0a8203e420552a0d409a6d58523e663f8ec857c0
Jeferson Sanches <jsanchesleao@gmail.com>
2026-02-23 09:04:50 +0100

    fixing some ts configs and css

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,18 @@
 import { defineCollection } from "astro:content";
 
-import { glob } from 'astro/loaders';
+import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
-const posts = defineCollection({
-  loader: glob({pattern: '**/*.md', base: './src/pages/posts'}),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    published: z.string(),
-  })
-})
+const postSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  published: z.string(),
+});
 
-export const collections = { posts }
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/pages/posts" }),
+  schema: postSchema,
+});
+
+export const collections = { posts };
+export type Post = z.infer<typeof postSchema>;
