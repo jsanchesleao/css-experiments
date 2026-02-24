f3e9385401b713837d048e59dd06b3798162eb28
Jeferson Sanches <jsanchesleao@gmail.com>
2026-02-24 13:42:58 +0100

    changing code highlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://jsanchesleao.github.io",
   base: "/css-experiments",
+  markdown: {
+    shikiConfig: {
+      theme: "gruvbox-dark-medium",
+    },
+  },
 });
